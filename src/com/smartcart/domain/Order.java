0744e5969812c01e489d12692ceb7d4efb5bef19
Sankaramma <31723651+Sankaramma@users.noreply.github.com>
2017-11-26 09:49:08 +0530

    added getter and setter in Order

--- src/com/smartcart/domain/Order.java
+++ src/com/smartcart/domain/Order.java
@@ -14,4 +14,46 @@ public class Order {
 	
 	private Address shippingAddress;
 
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getOrderDesc() {
+		return orderDesc;
+	}
+
+	public void setOrderDesc(String orderDesc) {
+		this.orderDesc = orderDesc;
+	}
+
+	public List<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+
+	public double getBillAmount() {
+		return billAmount;
+	}
+
+	public void setBillAmount(double billAmount) {
+		this.billAmount = billAmount;
+	}
+
+	public Address getShippingAddress() {
+		return shippingAddress;
+	}
+
+	public void setShippingAddress(Address shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+	
+	
+
 }
